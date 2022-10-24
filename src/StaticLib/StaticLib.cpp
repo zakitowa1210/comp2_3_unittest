3b7e69d957ca6980aa83d610d52d96b6a65aafe1
zakitowa1210 <114389339+zakitowa1210@users.noreply.github.com>
2022-10-24 15:53:24 +0900

    Update StaticLib.cpp

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -5,13 +5,34 @@
 // 2つの数字が等しいですか？
 bool is_same(int val1, int val2)
 {
-	// やること：この処理を数字を比較するように変更します
-	return false;
+	return val1 == val2;
 }
 
 // val1 から val3 の任意の組み合わせでsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3)
 {
-	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
-	return 0;
+	int flag = 0;
+	if(sum == val1){
+		flag++;
+	}
+	if(sum == val2){
+		flag++;
+	}
+	if(sum == val3){
+		flag++;
+	}
+	if(sum == val1+val2){
+		flag++;
+	}
+	if(sum == val1+val3){
+		flag++;
+	}
+	if(sum == val3+val2){
+		flag++;
+	}
+	if(sum == val3+val2+val1){
+		flag++;
+	}
+	
+	return flag;
 }
